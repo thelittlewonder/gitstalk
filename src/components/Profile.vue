8686c6cfa49f9a148481a8bc07ddc30109e710ee
Abhishek Sharma <hello.abhishek@outlook.com>
2018-07-10 00:15:54 +0530

    add API call and username validation

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
     <h1>User Profile</h1>
+    <div v-if="loading">
+      Loading...
+    </div>
+    <div v-else>
+      <div v-for="activity in result" :key="activity.id">
+        {{activity.id}}
+      </div>
+      <div v-if="result.length===0">Username does not exists</div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'Profile',
-  data () {
+  name: "Profile",
+  data() {
     return {
-      
-    }
+      result: [],
+      loading: true,
+      error: ""
+    };
   },
   mounted: function() {
-    console.log(this.$route.params.id);
+    let user = this.$route.params.id;
+    axios
+      .get("https://api.github.com/users/" + user + "/events")
+      .then(result => {
+        this.result = result.data;
+        this.loading = false;
+      })
+      .catch(err => {
+        this.error = err;
+      });
   }
-}
+};
 </script>
 
 <style scoped>
